a62819d171d80d2fb20d17e53f0063982170d358
danelleSchlebusch <schlebuschdanelle@gmail.com>
2026-08-05 12:59:36 +0200

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -83,3 +83,48 @@ def about():
         "author": "Danelle",
         "course": "CRUD API"
     }
+
+#-----------------------------------------------------------------
+#Stage 2: Read: list and a single task
+#-----------------------------------------------------------------
+
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Buy groceries",
+        "done": True
+    },
+    {
+        "id": 3,
+        "title": "Build CRUD API",
+        "done": False
+    }
+]
+
+@app.get("/")
+def read_root():
+    return {"message: Hello World"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+        
+    raise HTTPException(
+        status_code = 404,
+        detail = f"Task {task_id} not found"
+    )
