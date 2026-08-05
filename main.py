b0b88a5071f523eca9d96cc20a44d3be2f004ee5
danelleSchlebusch <schlebuschdanelle@gmail.com>
2026-08-05 15:56:44 +0200

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -61,7 +61,7 @@
 #Initialization
 #-------------------------------------------------------------------
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -159,3 +159,34 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+#---------------------------------------------------------------
+#Stage 4 - Update and Delete
+#---------------------------------------------------------------
+
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            if not updated_task.title.strip():
+                raise HTTPException(status_code=400, detail="Title cannot be empty")
+            
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+
+            return task
+
+        raise HTTPException(status_code=404, detail="Task not found")
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return Response(status_code=204)
+
+    raise HTTPException(status_code=404, detail="Task not found")
