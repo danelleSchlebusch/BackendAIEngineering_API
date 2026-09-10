957ad1e91d22d4164c8e27621698092bc714b9e1
danelleSchlebusch <schlebuschdanelle@gmail.com>
2026-09-10 14:35:20 +0200

    Stage 1: signup and login routes working

--- 04 Auth Login & Protection/main.py
+++ 04 Auth Login & Protection/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from supabase import create_client, Client
 
 load_dotenv()
@@ -13,7 +14,57 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 
+class AuthRequest(BaseModel):
+    email: str | None = None
+    password: str | None = None
 
 @app.get("/")
 def root():
     return {"message": "Server running and connected to Supabase"}
+
+#------------------------------------------------------------------
+# Sign Up
+#------------------------------------------------------------------
+
+@app.post("/auth/signup", status_code=201)
+def signup(data: AuthRequest):
+    if not data.email or not data.password:
+        raise HTTPException(
+            status_code=400,
+            detail="Email and password are required"
+        )
+
+    response = supabase.auth.sign_up({
+        "email": data.email,
+        "password": data.password
+    })
+
+    return response.user
+
+#---------------------------------------------------------------
+# Login
+#---------------------------------------------------------------
+
+@app.post("/auth/login")
+def login(data: AuthRequest):
+    if not data.email or not data.password:
+        raise HTTPException(
+            status_code=400,
+            detail="Email and password are required"
+        )
+
+    try:
+        response = supabase.auth.sign_in_with_password({
+                "email": data.email,
+                "password": data.password
+            })
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid login credentials"
+        )
+
+    return{
+        "access_token": response.session.access_token,
+        "refresh_token": response.session.refresh_token
+    }
